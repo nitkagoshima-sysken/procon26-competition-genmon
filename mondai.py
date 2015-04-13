5ed75df363d6747e7fce961e30ea92f1d6df3cca
ryo33 <ryo12redstone@gmail.com>
2015-04-13 20:03:02 +0900

    make it work

--- mondai.py
+++ mondai.py
@@ -9,8 +9,11 @@ block_size_max = 8
 block_cells_min = 4
 blocks_ratio_min = 0.7 #blank cells in field / cells of blocks 
 blocks_ratio_max = 1.5
-blocks_threshold_min = 0.01 #threshold of making random block
-blocks_threshold_max = 0.99
+blocks_threshold_min = 0.05 #threshold of making random block
+blocks_threshold_max = 0.50
+blocks_max = 200
+
+sys.setrecursionlimit(100000)
 
 def rand(threshold=0.5):
     return random.random() < threshold
@@ -23,20 +26,28 @@ def create(true="1", false="0"):
     field_width = (int)(random.random() * (field_size_max - field_size_min) + field_size_min)
     field_height = (int)(random.random() * (field_size_max - field_size_min) + field_size_min)
     field_threshold = random.random() * (field_threshold_max - field_threshold_min) + field_threshold_min
-    field = create_panel(field_width, field_height, field_threshold)
-    draw(field, true, false)
-    for i in range(0, 20):
-        print("")
-        draw(create_block(random.random() * (blocks_threshold_max - blocks_threshold_min) + blocks_threshold_min), true, false)
+    field, blank_blocks = create_panel(field_width, field_height, field_threshold)
+    cells_min = (int) (blank_blocks * (random.random() * (blocks_ratio_max - blocks_ratio_min) + blocks_ratio_min))
+    cells_sum = 0
+    blocks = []
+    while True:
+        block, cells = create_block(random.random() * (blocks_threshold_max - blocks_threshold_min) + blocks_threshold_min)
+        blocks.append(block)
+        cells_sum = cells_sum + cells
+        if cells_sum >= cells_min or len(blocks) == blocks_max:
+            break
+    return field, blocks
 
 def create_panel(field_width, field_height, field_threshold):
     field = [[rand(field_threshold) if (i < field_height and j < field_width) else True for j in range(0, field_size_max)] for i in range(0, field_size_max)]
+    if has_hole(get_groups(field, False)[0]):
+        return create_panel(field_width, field_height, field_threshold)
     n = 0
     for line in field:
         for item in line:
             if item:
                 n = n + 1
-    return field
+    return field, n
 
 def create_block(block_threshold):
     block = [[rand(block_threshold) for j in range(0, block_size_max)] for i in range(0, block_size_max)]
@@ -54,22 +65,27 @@ def create_block(block_threshold):
                 max[0].append(item);
     if max == None:
         return create_block(block_threshold)
-    result = cells_to_block(max[0])
+    result = cells_to_block(max[0][random.randint(0, len(max[0]) - 1)])
     negative_blocks, count = get_groups(result, False)
     if count > 0:
-        for negative_block in negative_blocks:
-            contact_with_edge = False
-            for cell in negative_block:
-                if cell[0] == 0 or cell[0] == block_size_max - 1 or cell[1] == 0 or cell[1] == block_size_max - 1:
-                    contact_with_edge = True
-                    break;
-            if not contact_with_edge:
-                return create_block(block_threshold)
-    return result
+        if has_hole(negative_blocks):
+            return create_block(block_threshold)
+    return result, max[1]
+
+def has_hole(blocks):
+    for block in blocks:
+        contact_with_edge = False
+        for cell in block:
+            if cell[0] == 0 or cell[0] == block_size_max - 1 or cell[1] == 0 or cell[1] == block_size_max - 1:
+                contact_with_edge = True
+                break
+        if not contact_with_edge:
+            return True
+    return False
 
 def cells_to_block(cells):
     result = [[False for j in range(0, block_size_max)] for i in range(0, block_size_max)]
-    for cell in cells[random.randint(0, len(cells) - 1)]:
+    for cell in cells:
         result[cell[0]][cell[1]] = True
     return result
 
@@ -105,4 +121,10 @@ def get_groups(block, target=True):
     return blocks, count
 
 if __name__ == "__main__":
-    create("▪︎", " ")
+    panel, blocks = create("▪︎", " ")
+    draw(panel)
+    print()
+    print(len(blocks))
+    for block in blocks:
+        draw(block)
+        print()
